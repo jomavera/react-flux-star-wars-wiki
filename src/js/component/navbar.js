4bd29fe59a35f462f414149e5fae4b8a30407cba
José Manuel Vera <josemanuel.vera.aray@gmail.com>
2022-02-11 12:11:52 +0000

    Update navabar component

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,16 +1,49 @@
 import React from "react";
+import { useContext, useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import swImage from "../../img/sw_logo.png";
+import { Context } from "../store/appContext";
 
 export const Navbar = () => {
+	const { store, actions } = useContext(Context);
+	const [favorite, setFavorite] = useState([]);
+
+
+	const favorites = store.favorites.filter((e) =>{
+		if (e.favorite) return e
+	})
+
+    const updateFavorite = (tipo, uid) => {
+        actions.changeFavorite(tipo, uid);
+		let favorites = store.favorites.filter((e) =>{
+			if (e.favorite) return e
+		})
+        setFavorite(favorites);
+    }
+
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
+		<nav className="navbar navbar-light bg-light mb-3 ps-3 pe-5">
 			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+				<div className="navbar-brand" >
+					<img src={swImage} alt="star wars logo" width="80px"/>
+				</div>
 			</Link>
 			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
+				<div className="dropdown">
+				<button className="btn dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+					Favorites <span className="badge bg-secondary">{favorites.length}</span>
+				</button>
+				<ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+					{favorites.map((e)=>{
+						return (
+						<li key={e.uid +"-"+ e.tipo}>
+							<div className="dropdown-item" >{e.nombre}</div>
+							<button type="button" className="btn btn btn-light" onClick={()=>{updateFavorite(e.tipo, e.uid)}}><i className="fa fa-trash" /></button>
+						</li>
+						)
+					})}
+				</ul>
+				</div>
 			</div>
 		</nav>
 	);

6c48caf3d5d61afddd3ce23947d2491ce1540d9c
José Manuel Vera <josemanuel.vera.aray@gmail.com>
2022-02-13 14:54:50 +0000

    Set store and actions in flux.js

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,42 +1,30 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+			favorites: []
 		},
 		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
+			addFavorite: (tipo, uid, nombre) => {
+				const store = getStore();
+				let store_ = store.favorites;
+				let store_f = store_.filter((e)=>{
+					if (e.uid === uid && e.tipo === tipo){
+						return e
+					}
+				});
+				if (store_f.length<1){
+					store_.push({tipo:tipo, uid:uid, nombre:nombre,favorite:false})
+				}
+				setStore({favorites:store_})
 			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
-			},
-			changeColor: (index, color) => {
-				//get the store
+			changeFavorite:(tipo, uid) =>{
 				const store = getStore();
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
+				const favorites = store.favorites.map((elm) => {
+					if (elm.uid === uid && elm.tipo === tipo) elm.favorite = !elm.favorite;
 					return elm;
 				});
-
-				//reset the global store
-				setStore({ demo: demo });
+				setStore({favorites:favorites});
 			}
 		}
 	};

134618805f45c3de9f17d46b9acbe1cf85611da8
José Manuel Vera <josemanuel.vera.aray@gmail.com>
2022-02-14 13:16:40 +0000

    Update layout for routing

--- src/js/layout.js
+++ src/js/layout.js
@@ -3,17 +3,15 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
 
 import { Home } from "./views/home";
-import { Demo } from "./views/demo";
 import { Single } from "./views/single";
+import { SinglePlanet } from "./views/singlePlanet";
 import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
 
-//create your first component
 const Layout = () => {
-	//the basename is used when your project is published in a subdirectory and not in the root of the domain
-	// you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
+
 	const basename = process.env.BASENAME || "";
 
 	return (
@@ -25,11 +23,11 @@ const Layout = () => {
 						<Route exact path="/">
 							<Home />
 						</Route>
-						<Route exact path="/demo">
-							<Demo />
-						</Route>
-						<Route exact path="/single/:theid">
+						<Route exact path="/single/:tipo/:theid">
 							<Single />
+						</Route>
+						<Route exact path="/singlePlanet/:tipo/:theid">
+							<SinglePlanet />
 						</Route>
 						<Route>
 							<h1>Not found!</h1>

c8ed7e9896048a50fd5154ef38972aa46f8ae9bd
José Manuel Vera <josemanuel.vera.aray@gmail.com>
2022-02-14 13:15:36 +0000

    Update views for single character and home

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,60 @@
 import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
-import "../../styles/home.css";
+import { useState, useEffect, useContext } from "react";
+import CardPeople from "../component/cardPeople";
+import CardPlanets from "../component/cardPlanets";
+import { Context } from "../store/appContext";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+export const Home = () => {
+	const { store, actions } = useContext(Context);
+	const [people, setPeople] = useState([]);
+	const [planets, setPlanets] = useState([]);
+
+	async function fetchPeople(){
+		const response = await fetch("https://www.swapi.tech/api/people/")
+		let data = await response.json();
+		return data.results;
+	}
+
+	async function fetchPlanets(){
+		const response = await fetch("https://www.swapi.tech/api/planets/")
+		let data = await response.json();
+		return data.results;
+	}
+
+
+	useEffect(()=>{
+		async function updateData(){
+			let peopleData = await fetchPeople();
+			let planetsData = await fetchPlanets();
+
+			setPeople(peopleData);
+			setPlanets(planetsData);
+			peopleData.forEach((e) =>{actions.addFavorite('people', e.uid, e.name)})
+			planetsData.forEach((e) =>{actions.addFavorite('planets', e.uid, e.name)})
+		};
+		
+		updateData();
+	},[])
+
+	return (
+		<div className="container-fluid">
+			<div className="display-4 text-danger p-1">
+				Characters
+			</div>
+			<div className="horizontal-scrollable">
+				<div className="row">
+					{ people.map((e)=> {return <CardPeople tipo="people" name={e.name} url={e.url} key={e.uid} uid={e.uid} />}) }
+				</div>
+			</div>
+			<div className="display-4 text-danger p-1">
+				Planets
+			</div>
+			<div className="horizontal-scrollable">
+				<div className="row">
+					{ planets.map((e)=> {return <CardPlanets tipo="planets" name={e.name} url={e.url} key={e.uid} uid={e.uid} />}) }
+				</div>
+			</div>
+		</div>
+		)
+
+};
--- src/js/views/single.js
+++ src/js/views/single.js
@@ -4,19 +4,89 @@ import { Link, useParams } from "react-router-dom";
 import { Context } from "../store/appContext";
 
 export const Single = props => {
+	
+	const [data, setData] = useState({});
 	const { store, actions } = useContext(Context);
 	const params = useParams();
+
+	async function fetchData(){
+		const response = await fetch(`https://www.swapi.tech/api/${params.tipo}/${params.theid}`)
+		let data = await response.json();
+        return data.result.properties
+	}
+	useEffect(()=>{
+		async function updateData(){
+			let data = await fetchData();
+			console.log(data);
+			setData(data);
+
+		}
+		updateData();
+	},[])
+
 	return (
-		<div className="jumbotron">
-			<h1 className="display-4">This will show the demo element: {store.demo[params.theid].title}</h1>
-
-			<hr className="my-4" />
-
-			<Link to="/">
-				<span className="btn btn-primary btn-lg" href="#" role="button">
-					Back home
-				</span>
-			</Link>
+		<div className="container">
+			<div className="row">
+				<div className="col-6">
+				<img
+                    src="https://via.placeholder.com/800x400"
+                    className="card-img-top"
+                    alt="photo"
+                />
+				</div>
+				<div className="col-6">
+					<div className="row">
+						<div className="col">
+							<div className="text-center display-5">
+								{data.name}
+							</div>
+						</div>
+					</div>
+					<div className="row">
+						<p className="lead">
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eu enim eu justo dictum commodo sit 
+						amet ac arcu. Phasellus consequat tempus tortor sit amet imperdiet. Phasellus quam turpis, 
+						tristique dapibus dolor ac, euismod vulputate felis. Sed venenatis magna sit amet fermentum porttitor.
+						 Pellentesque sollicitudin lorem posuere eros placerat, vitae mattis sapien feugiat. Class aptent taciti 
+						 sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Ut eros nibh, condimentum 
+						 non sapien at, ullamcorper fermentum tortor. Proin eu luctus mauris. Vivamus condimentum et quam 
+						 a rhoncus. Proin tortor arcu, malesuada id risus nec, egestas finibus tellus. Cras at accumsan justo. 
+						 Integer vel quam et nisi convallis pretium. Nullam malesuada vitae elit nec lobortis. Etiam nec 
+						 risus quis felis pretium feugiat vel eu metus. 
+						</p>
+						<br className="text-warning text-opacity-75"/>
+					</div>
+				</div>
+			</div>
+			<div className="row">
+			<hr className="text-danger text-opacity-75"/>
+			</div>
+			<div className="row text-danger">
+				<div className="col">
+					<p>Name</p>
+					<p className="fw-lighter">{data.name}</p>
+				</div>
+				<div className="col">
+					<p>Birth Year</p>
+					<p className="fw-lighter">{data.birth_year}</p>
+				</div>
+				<div className="col">
+					<p>Gender</p>
+					<p className="fw-lighter">{data.gender}</p>
+				</div>
+				<div className="col">
+					<p>Height</p>
+					<p className="fw-lighter">{data.height}</p>
+				</div>
+				<div className="col">
+					<p>Eye color</p>
+					<p className="fw-lighter">{data.eye_color}</p>
+				</div>
+				<div className="col">
+					<p>Skin Color</p>
+					<p className="fw-lighter">{data.skin_color}</p>
+				</div>
+			</div>
 		</div>
 	);
 };
